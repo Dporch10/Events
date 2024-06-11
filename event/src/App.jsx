efb89186f118c7c11498c58cced77245ca6b625e
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-11 03:12:09 -0400

    timing clock

--- event/src/App.jsx
+++ event/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./layout/Header/header";
 import Home from "./Components/Home/Home";
+import Conference from "./Components/Conference/conference";
 const App = () => {
     return (
         <>
-            <Header/>   
-            <Home/> 
+            <Header />   
+            <Home /> 
+            <Conference />
         </>
     );
 };
--- event/src/Components/Conference/Conference.jsx
+++ event/src/Components/Conference/Conference.jsx
@@ -1,10 +1,36 @@
+
+import "./Conference.css";
 const Conference = () => {
     return (
         <>
-            <h1>Hey Conference</h1>
+        <div className="container conference">
+            <div className="row justify-content-center">
+                <div className="col-sm-9">
+            <h6>Next Event Starts In</h6>
+            <p>
+                Get ready as our next conference will be coming around soon. The Countdown has begun! Mark your calendars and prepare for the special event. Stay tuned for updates and registration details. The Next Conference starts in:
+            </p>
+            <h6 id="ConferenceTimingClock"></h6>
+                </div>
+            </div>
+        </div>    
         </>
     );
     
 };
+    var CountDownDate = new Date("June 12, 2024").getTime();
+   //console.log(CountDownDate); 
+    var x = setInterval(() => {
+        var Now = new Date().getTime();
+        var Dis= CountDownDate - Now;
+
+        //console.log(Dis);
+        var Days = Math.floor(Dis/(1000 * 60 * 60 * 24));
+        var Hours = Math.floor((Dis % (1000 * 60 * 60 * 24)));
+        var Min = Math.floor((Dis % (1000 * 60 * 60)) / (1000 * 60));
+        var Sec = Math.floor ((Dis % (1000* 60)) / 1000);
+
+        document.getElementById("ConferenceTimingClock").innerHTML = Days + "D- " + Hours + "H- " + Min + "M- " + Sec + "S";
+    }, 1000)
 
     export default Conference;
