46ef43ae875714307fd268859e314893461262fc
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-11 03:59:02 -0400

    styling recent events

--- event/src/Components/RecentEvents/RecentEvents.jsx
+++ event/src/Components/RecentEvents/RecentEvents.jsx
@@ -4,12 +4,89 @@ const RecentEvents = () => {
         <>
             <div className="RecentEvents container">
                 <div className="content">
-                        <h6>
-                            Recent <span>Events</span>
-                        </h6>
                 </div>
+
+            <div className="row">
+                <div className="col-sm-4">
+                    </div>
+
+                    <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent1.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+
+                <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent2.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+
+                <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent3.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+            </div>
+
+            {/*second row*/}
+            <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent4.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+
+                <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent5.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+
+                <div className="col-sm-4">    
+                    <div className="box">
+                        <img src="/recent6.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="boxContent">
+                            <h6>Couples Retreat 2024</h6>
+                            <p>
+                                The Extravagant Couples Retreat in 2024. Couples were able to meet and greet and as well enjoy some piece and quiet on the beach.
+                            </p>
+                    </div>
+                </div>
+            
+
             </div>    
         </>
-    )
-}
+    );
+};
 export default RecentEvents;
